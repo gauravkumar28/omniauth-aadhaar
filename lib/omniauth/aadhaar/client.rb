1c11e42300c28f51797eb5cdd071d5c1f621cd73
Gaurav Kumar3 <gaurav.kumar3@C02QD5TFG8WP.local>
2016-12-09 10:13:40 +0530

    first commit

--- lib/omniauth/aadhaar/client.rb
+++ lib/omniauth/aadhaar/client.rb
@@ -1,2 +1,13 @@
 class Client
+  def initialize(params)
+    @uid = params[:usid]
+    @name = params[:name]
+    @email = params[:email]
+    @dob = params[:dob]
+  end
+
+  def authenticate!
+    ##############  Integrate with AADHAAR AUTH ######################
+    return true
+  end  
 end
